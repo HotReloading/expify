8f7d4ad197ca81b1bf6ba903ae547191a12eed9a
Priyanka <ranipriyankachennam933@gmail.com>
2026-03-12 17:25:54 +0530

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp.router(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: lightTheme,
       routerConfig: appRouter,
     );
